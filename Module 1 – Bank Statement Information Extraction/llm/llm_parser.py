c9672ef12b5f97c9b4890b92074b64e98197c694
knagaraj-2106 <nagarajkamale325@gmail.com>
2026-07-20 19:44:51 +0530

    Add files via upload

---
+++ Module 1 – Bank Statement Information Extraction/llm/llm_parser.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+from config import OPENAI_API_KEY
+
+client=OpenAI(api_key=OPENAI_API_KEY)
+
+def process_with_llm(data):
+    prompt=f"""
+    Analyze bank data:
+
+    {data}
+    """
+    response=client.chat.completions.create(
+
+    model="gpt-4o-mini",
+
+    messages=[
+    {
+    "role":"user",
+    "content":prompt
+    }
+    ]
+
+    )
+
+    return response.choices[0].message.content
